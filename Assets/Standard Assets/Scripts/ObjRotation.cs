6a8873c136974f11f50fe61e658dbb418b0cd306
N-vanDessel <n.vandessel@live.com>
2015-07-23 13:36:55 -0600

    last push before merge
    3d model of cable modem added + rotation script

---
+++ Assets/Standard Assets/Scripts/ObjRotation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjRotation : MonoBehaviour
+{
+	public float rotateVel;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		Rotate ();
+	}
+
+	void Rotate ()
+	{
+		transform.Rotate (Vector3.up * rotateVel);
+	}
+}
