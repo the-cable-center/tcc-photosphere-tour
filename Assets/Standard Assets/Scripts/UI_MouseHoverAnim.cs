c892d036b224dce4a3b723c8ff82acfc605a1f4f
N-vanDessel <n.vandessel@live.com>
2015-07-27 16:15:38 -0600

    Added UI interaction with archive. Created VR mouse. Added mouse lock ability.
    You can now lock the cursor to the center of the screen with an on
    screen UI, simply press ESC to release the cursor.
    
    Created a VR mouse that uses ray casting to interact with objects. Will
    eliminate the issue of the normal mouse not functioning normally with
    the Oculus Rift.
    
    You can now hover over the the archive object now and it will get
    illuminated using a sprite animation. This uses the new VR mouse to
    work.

---
+++ Assets/Standard Assets/Scripts/UI_MouseHoverAnim.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_MouseHoverAnim : MonoBehaviour
+{
+	public bool mouseOver;
+
+	private GameObject VRmouse;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+
+		VRmouse = GameObject.FindGameObjectWithTag ("vrMouse");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Keypad1)) {
+			anim.SetBool ("mouseOver", true);
+		} else {
+			anim.SetBool ("mouseOver", false);
+		}
+
+		if (VRmouse.GetComponent<VRmouse> ().hoverHit.collider.tag == "ArchiveModel"){
+			anim.SetBool ("mouseOver", true);
+		} else {
+			anim.SetBool ("mouseOver", false);
+		}
+	}
+	
+/* 
+	void OnMouseOver (Collider c)
+	{
+		if (c.gameObject.tag == "ArchiveModel") {
+			anim.SetBool ("mouseOver", true);
+		} else {
+			anim.SetBool ("mouseOver", false);
+		}
+	}
+
+	void OnMouseExit (Collider c)
+	{
+		if (c.gameObject.tag == "ArchiveModel") {
+			anim.SetBool ("mouseOver", false);
+		}
+	}
+*/
+
+}
---
+++ Assets/Standard Assets/Scripts/VRmouse.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRmouse : MonoBehaviour
+{
+	public RaycastHit clickHit;
+	public RaycastHit hoverHit;
+	
+	void FixedUpdate ()
+	{
+		Click ();
+		Hover ();
+	}
+
+	void Click ()
+	{
+		Ray click = new Ray (transform.position, transform.forward);
+
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.DrawRay (transform.position, transform.forward, Color.green);
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			if (Physics.Raycast(click, out clickHit, 65f)){
+				print ("Click_Object Found!");
+			}
+		}
+	}
+
+	void Hover ()
+	{
+		Ray hov = new Ray (transform.position, transform.forward);
+
+		Debug.DrawRay (transform.position, transform.forward, Color.red);
+		if (Physics.Raycast(hov, out hoverHit, 65f)){
+			print ("Click_Object Found!");
+		}
+	}
+}
