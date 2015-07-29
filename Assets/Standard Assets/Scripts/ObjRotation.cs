656bd71eda4f788370e485311444ac121daa1971
Nic-vanDessel <n.vandessel@live.com>
2015-07-29 11:51:14 -0600

    feature_archiveObjInteraction; click and drag
    Added ability to click and drag objects to affect their rotation.

--- Assets/Standard Assets/Scripts/ObjRotation.cs
+++ Assets/Standard Assets/Scripts/ObjRotation.cs
@@ -3,21 +3,45 @@ using System.Collections;
 
 public class ObjRotation : MonoBehaviour
 {
-	public float rotateVel;
+	public float passiveRotateVel;
 
+	private float rotationSpeed = 10f;
+	private float lerpSpeed = 1f;
+
+	private Vector3 theSpeed;
+	private Vector3 avgSpeed;
+	private bool isDragging;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+
+	}
+
+	void OnMouseDown ()
+	{
+		isDragging = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-
+		if (Input.GetMouseButton (0) && isDragging) {
+			theSpeed = new Vector3 (-Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"), 0.0F);
+			avgSpeed = Vector3.Lerp (avgSpeed, theSpeed, Time.deltaTime * 5);
+		} else {
+			if (isDragging) {
+				theSpeed = avgSpeed;
+				isDragging = false;
+			}
+			
+			float i = Time.deltaTime * lerpSpeed;
+			theSpeed = Vector3.Lerp (theSpeed, Vector3.zero, i);
+		}
+			transform.Rotate (Camera.main.transform.up * theSpeed.x * rotationSpeed, Space.World);
+			transform.Rotate (Camera.main.transform.right * theSpeed.y * rotationSpeed, Space.World);
 	}
-
+	
 	void FixedUpdate ()
 	{
 		Rotate ();
@@ -25,6 +49,9 @@ public class ObjRotation : MonoBehaviour
 
 	void Rotate ()
 	{
-		transform.Rotate (Vector3.up * rotateVel);
+
 	}
 }
+
+
+
