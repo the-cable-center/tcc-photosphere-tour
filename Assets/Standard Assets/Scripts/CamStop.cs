a6e05b0ec6d2f2769e9fdabea2ba8c9e586a36cb
Nic-vanDessel <n.vandessel@live.com>
2015-08-10 11:24:44 -0600

    :tada: feature_camLock complete!
    When clicking and dragging on an object the camera now locks its
    position so that you don't continue to look around while viewing the
    object.

---
+++ Assets/Standard Assets/Scripts/CamStop.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamStop : MonoBehaviour
+{
+
+    private GameObject archvObj;
+
+    public GameObject player;
+    public Camera main;
+    
+
+    void Start()
+    {
+        archvObj = GameObject.FindGameObjectWithTag("ArchiveModel");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && archvObj.GetComponent<ObjRotation>().isDragging)
+        {
+            player.GetComponent<MouseLook>().enabled = false;
+            main.GetComponent<MouseLook>().enabled = false;
+        }
+        else
+        {
+            if (archvObj.GetComponent<ObjRotation>().isDragging)
+            {
+                player.GetComponent<MouseLook>().enabled = true;
+                main.GetComponent<MouseLook>().enabled = true;
+            }
+            
+        }
+    }
+}
--- Assets/Standard Assets/Scripts/ObjRotation.cs
+++ Assets/Standard Assets/Scripts/ObjRotation.cs
@@ -3,54 +3,48 @@ using System.Collections;
 
 public class ObjRotation : MonoBehaviour
 {
-	public float passiveRotateVel;
 
-	private float rotationSpeed = 10f;
-	private float lerpSpeed = 1f;
+    public float rotationSpeed = 10f;
+    public float lerpSpeed = 1f;
 
-	private Vector3 theSpeed;
-	private Vector3 avgSpeed;
-	private bool isDragging;
+    private Vector3 theSpeed;
+    private Vector3 avgSpeed;
+    public bool isDragging;
 
-	// Use this for initialization
-	void Start ()
-	{
+    // Use this for initialization
+    void Start()
+    {
 
-	}
+    }
 
-	void OnMouseDown ()
-	{
-		isDragging = true;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (Input.GetMouseButton (0) && isDragging) {
-			theSpeed = new Vector3 (-Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"), 0.0F);
-			avgSpeed = Vector3.Lerp (avgSpeed, theSpeed, Time.deltaTime * 5);
-		} else {
-			if (isDragging) {
-				theSpeed = avgSpeed;
-				isDragging = false;
-			}
-			
-			float i = Time.deltaTime * lerpSpeed;
-			theSpeed = Vector3.Lerp (theSpeed, Vector3.zero, i);
-		}
-			transform.Rotate (Camera.main.transform.up * theSpeed.x * rotationSpeed, Space.World);
-			transform.Rotate (Camera.main.transform.right * theSpeed.y * rotationSpeed, Space.World);
-	}
-	
-	void FixedUpdate ()
-	{
-		Rotate ();
-	}
+    void OnMouseDown()
+    {
+        isDragging = true;
+    }
 
-	void Rotate ()
-	{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && isDragging)
+        {
+            theSpeed = new Vector3(-Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0.0F);
+            avgSpeed = Vector3.Lerp(avgSpeed, theSpeed, Time.deltaTime * 5);
+        }
+        else
+        {
+            if (isDragging)
+            {
+                theSpeed = avgSpeed;
+                isDragging = false;
+            }
 
-	}
+            float i = Time.deltaTime * lerpSpeed;
+            theSpeed = Vector3.Lerp(theSpeed, Vector3.zero, i);
+        }
+        transform.Rotate(Camera.main.transform.up * theSpeed.x * rotationSpeed, Space.World);
+        transform.Rotate(Camera.main.transform.right * theSpeed.y * rotationSpeed, Space.World);
+    }
+
 }
 
 
